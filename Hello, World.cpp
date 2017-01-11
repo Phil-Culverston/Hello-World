10091a8214c0817bb4dfc76aec09c81c7db073d6
Phil-Culverston <tng2471@gmail.com>
2017-01-11 22:35:03 +0000

    Add files via upload

---
+++ Hello, World.cpp
@@ -0,0 +1,11 @@
+// stdafx.h and iostream headers.
+
+#include "stdafx.h"								
+#include <iostream>
+
+int main() // Designated entry point initialize main. cannot be called recursively, additionaly address cannot be taken.
+{
+	std::cout << "Hello, World!\n"; // Character out. Prints Hello, World!
+    return 0; // Return statement, terminates execution of code and returns control.
+}
+
