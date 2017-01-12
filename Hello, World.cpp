05ee45ea44b502779ef5855bee31a374a385833b
Phil-Culverston <tng2471@gmail.com>
2017-01-12 01:08:24 +0000

    Update Hello, World.cpp

--- Hello, World.cpp
+++ Hello, World.cpp
@@ -1,4 +1,5 @@
-// Headers
+//Headers
+
 #include "stdafx.h"
 #include "../../std_lib_facilities.h"
 
@@ -6,5 +7,6 @@
 int main() // Designated entry point initialize main. cannot be called recursively, additionaly address cannot be taken.
 {
 	cout << "Hello, World!\n"; // Character out. Prints Hello, World!
+	keep_window_open(); //Holds the console window open until the user enters a character. (modifying of this further to instead use the enter key is planned.)
 	return 0; // Return statement, terminates execution of code and returns control.
 }
