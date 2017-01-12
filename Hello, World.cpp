a0bdba5d25f6b2a06a99ad15571c0c7495542e11
Phil-Culverston <tng2471@gmail.com>
2017-01-12 00:34:02 +0000

    Update Hello, World.cpp

--- Hello, World.cpp
+++ Hello, World.cpp
@@ -1,11 +1,9 @@
-// stdafx.h and iostream headers.
+#include "../../std_lib_facilities.h"
+#include "stdafx.h"
 
-#include "stdafx.h"								
-#include <iostream>
 
 int main() // Designated entry point initialize main. cannot be called recursively, additionaly address cannot be taken.
 {
-	std::cout << "Hello, World!\n"; // Character out. Prints Hello, World!
-    return 0; // Return statement, terminates execution of code and returns control.
+	cout << "Hello, World!\n"; // Character out. Prints Hello, World!
+	return 0; // Return statement, terminates execution of code and returns control.
 }
-
