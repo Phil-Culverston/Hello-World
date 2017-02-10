c89c95ab65ae73838b021a13e7ac6cbae00a57f4
Phil-Culverston <tng2471@gmail.com>
2017-02-10 16:19:17 +0000

    Update Hello, World.cpp

--- Hello, World.cpp
+++ Hello, World.cpp
@@ -1,5 +1,5 @@
 //Headers
-//
+
 #include "stdafx.h"
 #include "../../std_lib_facilities.h"
 
@@ -7,6 +7,10 @@
 int main() // Designated entry point initialize main. cannot be called recursively, additionaly address cannot be taken.
 {
 	cout << "Hello, World!\n"; // Character out. Prints Hello, World!
-	keep_window_open(); //Holds the console window open until the user enters a character. (modifying of this further to instead use the enter key is planned.)
+		cout << "Please enter your first name (followed by 'enter'):\n";
+		string first_name;	//first_name is a variable of type string.
+		cin >> first_name;	//read characters into first_name.
+		cout << "Hello, " << first_name << "!\n";
+	keep_window_open(); //Waits for a character to be entered before exiting.
 	return 0; // Return statement, terminates execution of code and returns control.
 }
