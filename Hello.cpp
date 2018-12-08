1abfff04ba1bb39be512b50741102a55aae44b42
Phil-Culverston <tng2471@gmail.com>
2018-12-08 10:06:50 +0000

    Add files via upload
    A simple hello world program. Used by the book in order to demonstrate the importance of grammar when programming. In addition to explaining how headers work (in this instance the C++ standard library header or std_lib_facilities.h).

---
+++ Hello.cpp
@@ -0,0 +1,10 @@
+// This is a basic hello world program
+
+#include "pch.h"
+#include "std_lib_facilities.h"
+
+int main()	//C++ programs start by executing the function main.
+{
+	cout<<"Hello, World!\n"; //output hello world (character output stream cout).
+	return 0;
+}
